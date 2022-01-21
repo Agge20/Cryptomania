54a79d8e214934bf9a674f5b74272688e2f660b0
August <augustraro112@hotmail.com>
2022-01-22 00:24:14 +0100

    Project skeleton
    Installed Vue 3.js, Vue-Router, Vuex 4, Firebase 9 and tailwindcss

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from "vue";
+
+// import vue-router
+import { createRouter, createWebHistory } from "vue-router";
+
+// import vuex 4 store
+import store from "./store/store.js";
+
+import App from "./App.vue";
+
+// global styling
+import "./index.css";
+
+// route views
+import Home from "./views/Home.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/home",
+      name: "Home",
+      component: Home,
+    },
+  ],
+});
+
+createApp(App).use(router).use(store).mount("#app");
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  // global state
+  state: {},
+  // to change/update/mutate state
+  mutations: {},
+  // to run async-functions code, (here we destructure the payload)
+  actions: {},
+});
+
+// export store
+export default store;
