c2a0c649e9d5e9dd39961e73e2d3ab53942d9870
august-ubuntu <augustraro112@hotmail.com>
2022-02-20 19:28:08 +0100

    Made News-Snapshot a little more responsive.

--- src/hooks/get/useGetTickerTapeCoins.js
+++ src/hooks/get/useGetTickerTapeCoins.js
@@ -7,7 +7,6 @@ const useGetTickerTapeCoins = () => {
 
   // get data
   const getCoins = async () => {
-    console.log("fetched data");
     // this url returns 15 of highest ranking coins
     const URL =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=false";
--- src/views/subviews/NewsSnapshot.vue
+++ src/views/subviews/NewsSnapshot.vue
@@ -9,10 +9,9 @@
         </h2>
       </div>
     </div>
-    <div class="flex flex-col items-center justify-center mt-36">
-      <span v-if="error" class="mx-auto text-theme_red text-2xl">{{
-        error
-      }}</span>
+    <div
+      class="flex flex-col items-center justify-center w-full mt-36 border-red-500 border-2"
+    >
       <div v-if="loading">
         <div
           class="flex flex-wrap content-start flex-col lg:flex-row justify-end"
@@ -25,19 +24,35 @@
           </div>
         </div>
       </div>
-      <div
-        v-if="!loading"
-        class="flex flex-wrap content-start flex-col lg:flex-row justify-end"
-      >
-        <div v-for="article in newsData" class="m-4 last:hidden xl:last:flex">
-          <NewsCard :article="article" />
+      <div class="flex flex-col justify-center">
+        <div
+          v-if="!loading"
+          class="flex flex-wrap content-start flex-col lg:flex-row justify-end"
+        >
+          <div v-for="article in newsData" class="m-4 last:hidden xl:last:flex">
+            <NewsCard :article="article" />
+          </div>
+          <span v-if="error" class="mx-auto text-theme_red text-2xl">{{
+            error
+          }}</span>
         </div>
-      </div>
-      <div class="w-full flex justify-end pr-4">
-        <LinkButton
-          :text="{ data: 'More News' }"
-          class="w-56 h-3/4 flex justify-center"
-        />
+        <div v-if="newsData.length">
+          <div class="w-full flex justify-end pr-4 border-2 border-blue-600">
+            <LinkButton
+              :text="{ data: 'More News' }"
+              class="w-56 h-3/4 flex justify-center"
+            />
+          </div>
+        </div>
+        <div
+          v-else
+          class="w-full flex justify-center pr-4 border-2 border-blue-600"
+        >
+          <LinkButton
+            :text="{ data: 'More News' }"
+            class="w-56 h-3/4 flex justify-center"
+          />
+        </div>
       </div>
     </div>
   </section>
