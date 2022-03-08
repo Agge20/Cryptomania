3da0961729a30c184f7776a13536a6a35b17b5d8
agge-ubuntu-ideapad-5 <augustraro112@hotmail.com>
2022-03-08 17:07:17 +0100

    Created use marketData hook and rendered data in template

--- src/components/market/MarketItem.vue
+++ src/components/market/MarketItem.vue
@@ -1,30 +1,33 @@
 <template>
-  <div
-    class="p-0 border-2 border-red-600 m-0 text-2xl grid grid-cols-7 auto-cols-min"
-  >
-    <div class="h-14 flex items-center p-1 border-2 border-red-600">
-      <img
-        class="h-2/3"
-        src="http://assets.stickpng.com/images/5a521fa72f93c7a8d5137fcf.png"
-        alt=""
-      />
-      <p class="ml-2 font-roboto font-medium">Bitcoin - BTC</p>
+  <div class="p-0 m-0 text-2xl grid grid-cols-7">
+    <div class="h-14 flex items-center p-2 ml-2">
+      <img class="h-2/3" :src="coinData.image" alt="" />
+      <p class="ml-2 font-roboto font-medium">{{ coinData.name }}</p>
     </div>
-    <p class="border-2 border-red-600">$44328</p>
-    <p class="border-2 border-red-600">+6.8%</p>
-    <p class="border-2 border-red-600">$45390</p>
-    <p class="border-2 border-red-600">$43390</p>
-    <p class="border-2 border-red-600">$837312729282</p>
-    <p class="border-2 border-red-600">No. 1</p>
+    <p class="market-column-item">{{ coinData.current_price }}</p>
+    <p class="market-column-item">{{ coinData.price_change_24h }}</p>
+    <p class="market-column-item">$45390</p>
+    <p class="market-column-item">$43390</p>
+    <p class="market-column-item">$837312729282</p>
+    <p class="market-column-item ml-auto mr-4">No. 1</p>
   </div>
 </template>
 
 <script>
 export default {
+  props: ["coinData"],
   setup() {
     return {};
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url("../../index.css");
+
+@layer components {
+  .market-column-item {
+    @apply my-auto;
+  }
+}
+</style>
--- src/views/sections/Market.vue
+++ src/views/sections/Market.vue
@@ -1,17 +1,15 @@
 <template>
-  <section class="flex items-center">
+  <section class="flex items-start">
     <div
       class="w-96 flex items-center justify-center whitespace-nowrap relative left-0 bg-transparent"
     >
       <LargeHeader
         :text="{ data: 'THE MARKET' }"
         :theme="{ light: true }"
-        class="rotate-90"
+        class="rotate-90 vertical-column-header"
       />
     </div>
-    <div
-      class="border-2 border-theme_dark_purple w-full min-h-screen flex flex-col bg-theme_white"
-    >
+    <div class="w-full min-h-screen flex flex-col bg-theme_white">
       <div
         class="bg-theme_dark_purple text-theme_white text-4xl font-montserrat uppercase font-semibold pl-2 pb-0 pt-16 grid grid-cols-7 auto-cols-min"
       >
@@ -30,25 +28,40 @@
           <span class="text-base">24H</span>
         </div>
         <p class="market-col-header">Marketcap</p>
-        <p class="market-col-header">Rank</p>
+        <p class="market-col-header ml-auto mr-4">Rank</p>
       </div>
-
-      <MarketItem />
+      <div v-for="coinData in marketData">
+        <MarketItem :coinData="coinData" />
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+// vue imports
+import { ref } from "vue";
+
 // components
 import LargeHeader from "../../components/headers/LargeHeader.vue";
 import MarketItem from "../../components/market/MarketItem.vue";
+
+// hooks
+import useGetMarketData from "../../hooks/get/market/useGetMarketData";
 
 export default {
   components: {
     LargeHeader,
     MarketItem,
   },
-  setup() {},
+  setup() {
+    const { getMarketData, marketData, loading, error } = useGetMarketData();
+    const PAGE = ref(1);
+
+    getMarketData(PAGE.value);
+    return {
+      marketData,
+    };
+  },
 };
 </script>
 
