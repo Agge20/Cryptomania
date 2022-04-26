dfca578549c747eb6650505d994ab715f40b0b13
august-ronnle <augustraro112@hotmail.com>
2022-04-26 15:43:36 +0200

    sortByMarketcap now works as intended

--- src/hooks/market/useSortByMarketcap.js
+++ src/hooks/market/useSortByMarketcap.js
@@ -1,3 +1,4 @@
+/*
 import { ref } from "vue";
 
 const useSortByMarketcap = () => {
@@ -5,13 +6,7 @@ const useSortByMarketcap = () => {
     const sortedData = ref([]);
     let shouldAsc = true;
 
-    const sortByMarketcap = (data) => {
-        // on render save original data
-        /*
-            1 = original data
-            2 = descending data
-            3 = ascending data
-        */
+    const sortByMarketcap = (data, shouldChangeDesc) => {
         if (shouldAsc) {
             sortedData.value = [...data];
             sortedData.value.sort((a, b) => {
@@ -26,6 +21,48 @@ const useSortByMarketcap = () => {
         } else {
             returnData.value.reverse();
             shouldAsc = true;
+        }
+    };
+    return {
+        returnData,
+        sortByMarketcap,
+    };
+};
+
+export default useSortByMarketcap;
+*/
+
+import { ref } from "vue";
+
+const useSortByMarketcap = () => {
+    const returnData = ref([]);
+    const sortedData = ref([]);
+    const desc = ref(false);
+    const ascData = ref([]);
+    const descData = ref([]);
+
+    const sortByMarketcap = (data, shouldChangeDesc) => {
+        // if user clicked name or it was a refetch
+        shouldChangeDesc ? (desc.value = !desc.value) : null;
+        sortedData.value = [...data];
+
+        sortedData.value.sort((a, b) => {
+            if (parseFloat(a.market_cap) >= parseFloat(b.market_cap)) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        descData.value = [...sortedData.value];
+        ascData.value = [...descData.value].reverse();
+
+        // if descending data
+        if (desc.value) {
+            returnData.value = descData.value;
+        }
+        // if ascending data
+        else {
+            returnData.value = ascData.value;
         }
     };
     return {
